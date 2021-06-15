141c9158790263566432df2952b54cde8e02bfb3
藤露 <080301087@163.com>
2021-06-15 12:21:05 +0800

    Update 02.07-numpy数组索引的高级应用.py

--- 02.07-numpy数组索引的高级应用.py
+++ 02.07-numpy数组索引的高级应用.py
@@ -18,4 +18,22 @@ X = np.arange(12).reshape((3, 4))
 print("X:  ",X)
 row = np.array([0, 1, 2])
 col = np.array([2, 1, 3])
-print("X[row, col]:  ",X[row, col])
+print("X[row, col]:  ",X[row, col])  #第1个值是X[0, 2], 第2个值是X[1, 1],第3个值是X[2, 3]
+print("X[row[:, np.newaxis], col]:  ",X[row[:, np.newaxis], col])
+print("row[:, np.newaxis] * col:  ",row[:, np.newaxis] * col)
+
+# np.newaxis的作用就是增加一个维度
+x1 = np.array([1, 2, 3, 4, 5])
+x1_new = x1[:, np.newaxis]
+print("x1_new:  ",x1_new)
+x1_new = x1[np.newaxis,:]
+print("x1_new:  ",x1_new)
+
+# 混合索引
+print("X:  ",X)
+print("X[2, [2, 0, 1]]:  ",X[2, [2, 0, 1]])
+print("X[1:, [2, 0, 1]]:  ",X[1:, [2, 0, 1]])
+mask = np.array([1, 0, 1, 0], dtype=bool)
+print("XX[row[:, np.newaxis], mask]:  ",X[row[:, np.newaxis], mask])
+
+
