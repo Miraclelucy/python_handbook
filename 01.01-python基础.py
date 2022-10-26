a1f532fe4d2048ac7c5f75f206116343b08ee802
藤露 <080301087@163.com>
2022-10-26 13:52:16 +0800

    Update 01.01-python基础.py

--- 01.01-python基础.py
+++ 01.01-python基础.py
@@ -1,4 +1,5 @@
 # 1. Python Dictionary get() Method
+# 1.1 return the value of an item that exist
 car = {
   "brand": "Ford",
   "model": "Mustang",
@@ -10,4 +11,20 @@ x = car.get("model")
 print(x)
 # Mustang
 
-# 2.
+# 1.2. Try to return the value of an item that do not exist
+car = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+x = car.get("price", 15000)
+
+print(x)
+# 15000
+
+# 1.3. dictionary.get(keyname, value)
+# Parameter	Description
+# keyname -	Required. The keyname of the item you want to return the value from
+# value -	Optional. A value to return if the specified key does not exist. Default value None
+
