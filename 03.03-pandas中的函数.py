126c669badae268d59266f35b9ff47a0cfe8619f
藤露 <080301087@163.com>
2022-11-10 14:47:37 +0800

    Update 03.03-pandas中的函数.py

--- 03.03-pandas中的函数.py
+++ 03.03-pandas中的函数.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
-### count函数方法的使用
+import matplotlib.pyplot as plt
+### count()函数方法的使用
 ### 计算每列或每行的非NA单元格。
 
 df = pd.DataFrame({"Person":
@@ -10,3 +11,11 @@ df = pd.DataFrame({"Person":
 
 print(df.count()) # 对于每个列，non-NA/null条目的数量。
 print(df.count(axis='columns')) # 对于每个行，non-NA/null条目的数量。
+
+### value_counts()函数方法的使用
+index = pd.DataFrame([3, 1, 2, 3, 4, np.nan], columns=['age'])
+idx = index.value_counts() # 返回的是一个series
+ax=plt.subplot(1,2,1)
+idx.plot(kind='pie',autopct='%1.1f%%',shadow=True) # 画饼图
+plt.show()
+print(idx)
