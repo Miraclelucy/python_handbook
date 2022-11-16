81c93af60f32e8beeff12f477e949b0260933cc6
藤露 <080301087@163.com>
2022-11-16 18:47:10 +0800

    Create 02.00-numpy简介.py

---
+++ 02numpy/02.00-numpy简介.py
@@ -0,0 +1,22 @@
+"""
+NumPy(Numerical Python) 是 Python 语言的一个扩展程序库，支持大量的维度数组与矩阵运算，
+此外也针对数组运算提供大量的数学函数库。
+
+NumPy 的前身 Numeric 最早是由 Jim Hugunin 与其它协作者共同开发，
+2005 年，Travis Oliphant 在 Numeric 中结合了另一个同性质的程序库 Numarray 的特色，
+并加入了其它扩展而开发了 NumPy。NumPy 为开放源代码并且由许多协作者共同维护开发。
+
+NumPy 是一个运行速度非常快的数学库，主要用于数组计算，
+通常与 SciPy（Scientific Python）和 Matplotlib（绘图库）一起使用， 这种组合广泛用于替代 MatLab，
+是一个强大的科学计算环境，有助于我们通过 Python 学习数据科学或者机器学习。
+
+SciPy 是一个开源的 Python 算法库和数学工具包。
+SciPy 包含的模块有最优化、线性代数、积分、插值、特殊函数、快速傅里叶变换、信号处理和图像处理、
+常微分方程求解和其他科学与工程中常用的计算。
+
+Matplotlib 是 Python 编程语言及其数值数学扩展包 NumPy 的可视化操作界面。
+它为利用通用的图形用户界面工具包，如 Tkinter, wxPython, Qt 或 GTK+
+向应用程序嵌入式绘图提供了应用程序接口（API）。
+
+"""
+
